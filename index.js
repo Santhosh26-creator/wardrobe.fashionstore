92b2c6f41e80bc5153b750b8cb4cbecd2095221b
Santhosh26-creator <84014178+Santhosh26-creator@users.noreply.github.com>
2024-04-29 23:52:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+function showNavbar()
+{
+    document.querySelector(".side-navbar").style.left="0"
+}
+
+function closeNavbar()
+{
+    document.querySelector(".side-navbar").style.left="-60%";
+}
